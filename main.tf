90bf9615f660b0618e503380181172d002215c39
akihirosasaki <akihirosasaki0730@gmail.com>
2022-01-13 00:32:46 +0900

    add embulk files

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 # s3, bigqueryの環境構成を指定
 
 resource "google_bigquery_dataset" "bigquery_dataset" {
-  dataset_id    = "asasaki-data-infra-dataset"
-  friendly_name = "asasaki-data-infra-dataset"
+  dataset_id    = "asasaki_data_infra_dataset"
+  friendly_name = "asasaki_data_infra_dataset"
   location      = "asia-northeast1"
 }
 
 resource "google_bigquery_table" "bigquery_table" {
   dataset_id          = google_bigquery_dataset.bigquery_dataset.dataset_id
-  table_id            = "asasaki-data-infra-table"
+  table_id            = "asasaki_data_infra_table"
   schema              = file("./bigquery/schema_asasaki-data-infra-table.json")
   deletion_protection = false
 }
